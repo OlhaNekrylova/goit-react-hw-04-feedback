4852c53963675359ee09705717a40caeae1f8944
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-02-07 01:01:44 +0200

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function App () {
+  const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [positivePercentage] = useState(0); 
 
   countTotalFeedback = () => {
     const {good, neutral, bad } = this.state;
@@ -60,5 +61,3 @@ class App extends React.Component {
     );
   };
 };
-
-export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,30 +1,18 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-const Statistics = ({
-    good, 
-    neutral, 
-    bad,
-    total, 
-    positivePercentage
-}) => (
-    <>
-        <ul className={css.statistics}>
-            <li className={css.options}>Good: {good}</li>
-            <li className={css.options}>Neutral: {neutral}</li>
-            <li className={css.options}>Bad: {bad}</li>
-            <li className={css.options}>Total: {total}</li>
-            <li className={css.options}>Positive feedback: {positivePercentage}%</li>
-        </ul>
-    </>
-);
+export default function Statistics ({good,neutral, bad, total, positivePercentage}) {
+      
 
-Statistics.propTypes = {
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired,
+    return (
+        <>
+            <ul className={css.statistics}>
+                <li className={css.options}>Good: {good}</li>
+                <li className={css.options}>Neutral: {neutral}</li>
+                <li className={css.options}>Bad: {bad}</li>
+                <li className={css.options}>Total: {total}</li>
+                <li className={css.options}>Positive feedback: {positivePercentage}%</li>
+            </ul>
+        </>
+    );
 };
-
-export default Statistics;
